f490372c96dfc271422c38fbb8aa348646b8c61c
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-11-22 21:44:24 +0300

    new lesson

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
+import input from './modules/input'
 
 timer('22 november 2021')
 menu()
 modal()
+input()

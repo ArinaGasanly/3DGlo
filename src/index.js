f1b3a6e25f002b69a6b3ffc6c5a8cc001738fc93
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-11-27 18:14:47 +0300

    добавление 22 урока

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
 import inputCheck from './modules/input'
+import tabs from './modules/tabs'
 
 timer('22 november 2021')
 menu()
 modal()
 inputCheck()
+tabs()
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,7 +1,6 @@
 const modal = () => {
   const modal = document.querySelector('.popup')
   const buttons = document.querySelectorAll('.popup-btn')
-  const closeBtn = modal.querySelector('.popup-close')
 
 
   
@@ -35,8 +34,11 @@ const addOpacity = () => {
   })
 })
 
-  closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none'
+  modal.addEventListener('click', (e) => {
+    if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
+      modal.style.display = 'none'
+    }
+      
   })
 
 }

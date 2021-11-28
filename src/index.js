518be05f7ac4656132b9985ad3a85166148a4d65
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-11-28 15:01:39 +0300

    добавление 23 урока по видео

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from './modules/menu'
 import modal from './modules/modal'
 import inputCheck from './modules/input'
 import tabs from './modules/tabs'
+import slider from './modules/slider'
 
 timer('22 november 2021')
 menu()
 modal()
 inputCheck()
 tabs()
+slider()

ed9a3972bcde715b4ae81fa94ffaa5f75e99fceb
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-11-29 20:31:10 +0300

    добавление 24 урока

---
+++ src/modules/input.js
@@ -0,0 +1,34 @@
+const inputCheck = () => {
+  const calculator = document.querySelectorAll('.calc-block')
+  const inputName = document.querySelectorAll('.form-name')
+  const inputEmail = document.querySelectorAll('.form-email')
+  const inputPhone = document.querySelectorAll('.form-phone')
+
+  calculator.forEach(value => {
+    value.addEventListener('input', (e) => {
+      e.target.value = e.target.value.replace(/\D+/g, '')
+    })
+  })
+
+  inputName.forEach(value => {
+    value.addEventListener('input', (e) => {
+      e.target.value = e.target.value.replace(/[^[а-яё\s-]/gi, '')
+    })
+  })
+
+  inputEmail.forEach(value => {
+    value.addEventListener('input', (e) => {
+      e.target.value = e.target.value.replace(/[^[a-zA-Z_@.!~*' \-]*/g, '')
+    })
+  })
+
+  inputPhone.forEach(value => {
+    value.addEventListener('input', (e) => {
+      e.target.value = e.target.value.replace(/[^[0-9()-]*/g, '')
+    })
+  })
+
+
+}
+
+export default inputCheck
---
+++ src/modules/menu.js
@@ -0,0 +1,21 @@
+const menu = function () {
+
+  const menuBtn = document.querySelector('.menu');
+  const menu = document.querySelector('menu');
+
+  const handleMenu = () => {
+
+    menu.classList.toggle('active-menu');
+  }
+
+  menuBtn.addEventListener('click', handleMenu);
+  menu.addEventListener('click', (e) => {
+    console.log();
+    if (e.target.classList.contains('close-btn') || e.target.closest('ul>li>a')) {
+      handleMenu();
+    }
+  })
+}
+
+
+export default menu

8a5e04e579671e6f0411e696da5e0d84171f5754
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-11-30 21:55:23 +0300

    добавление 25 урока

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import oneModule from './modules/one'
-import twoModule from './modules/two'
+import timer from './modules/timer'
+import menu from './modules/menu'
+import modal from './modules/modal'
+import inputCheck from './modules/input'
+import tabs from './modules/tabs'
+import slider from './modules/slider'
+import calc from './modules/calc'
 
-oneModule()
-twoModule()
+timer('22 november 2021')
+menu()
+modal()
+inputCheck()
+tabs()
+slider()
+calc()
